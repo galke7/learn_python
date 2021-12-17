9b517d3aaf9a3290acd32ba5935fdaff88255035
galke7 <73199341+galke7@users.noreply.github.com>
2021-12-17 21:45:42 +0200

    new drill for ex14

--- ex14.py
+++ ex14.py
@@ -1,9 +1,9 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, user_name, user_last_name = argv
+prompt = '~: '
 
-print(f"Hi {user_name}, I'm the {script} script.")
+print(f"Hi {user_name} {user_last_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
