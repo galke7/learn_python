291dac9697de923caa9034c7f4a9085d0c562a08
galke7 <73199341+galke7@users.noreply.github.com>
2021-12-17 05:32:04 +0200

    added fourth variable to be raise by user input

--- ex13drill.py
+++ ex13drill.py
@@ -1,11 +1,16 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, first, second = argv
+script, first, second, third, fourth = argv
 
 print("The script is called: ", script)
-print("Your first variable is: ", first)
-print("Your second variable is: ", second)
+print("My first variable is: ", first)
+print("My second variable is: ", second)
+print("My third variable is: ", third)
+print("My last var for Now is.. ", fourth)
+
 multiplay_by = int(input("enter number to multiply by: "))
-print(f"first num{first} multiply by {multiplay_by}")
-print(f" {int(first) * multiplay_by}")
+print(f"first num {first} multiply by {multiplay_by}:")
+print(int(first) * multiplay_by)
+raise_by = int(input("enter number to raise by: "))
+print(f"fourth num {fourth} raise to the power of {raise_by}:\n{int(fourth) ** raise_by}")
 
