90a23a7e9f3e1e153a279047e48149666b97bd93
galke7 <73199341+galke7@users.noreply.github.com>
2021-12-18 07:10:35 +0200

    added comments to ex15.py

--- ex15.py
+++ ex15.py
@@ -1,17 +1,18 @@
+# imorting argv module to get filename parameter befor the script run
 from sys import argv
-
+# unpack parameters from argv to variable
 script, filename = argv
-
+# open file and assign txt as its handler
 txt = open(filename)
-
+# read the file and printing it to the screen
 print(f"Here's your file {filename}:")
 print(txt.read())
 
 print("Type the filename again:")
 
 file_again = input("> ")
-
+# open anather file from user input
 txt_again = open(file_again)
-
+# read the txt file and print it
 print(txt_again.read())
 
